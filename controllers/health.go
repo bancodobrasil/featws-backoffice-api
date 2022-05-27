4877879d85ee33863b8af4405acb4cd0a828e59c
C1325543 Matheus Romano Braga <romanobraga.m@gmail.com>
2022-05-27 16:44:30 -0300

    fix: url parser

--- controllers/health.go
+++ controllers/health.go
@@ -1,15 +1,18 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
+	"net/url"
 	"time"
 
+	"github.com/bancodobrasil/featws-api/config"
 	"github.com/bancodobrasil/featws-api/database"
 	"github.com/bancodobrasil/healthcheck"
 	"github.com/bancodobrasil/healthcheck/checks/db"
 	"github.com/bancodobrasil/healthcheck/checks/goroutine"
-	healthHttp "github.com/bancodobrasil/healthcheck/checks/http"
 	"github.com/gin-gonic/gin"
+	"github.com/gsdenys/healthcheck/checks"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -26,6 +29,10 @@ func NewHealthController() *HealthController {
 }
 
 func newHandler() healthcheck.Handler {
+	cfg := config.GetConfig()
+	rawGitlabURL := cfg.GitlabURL
+	gitlab, _ := url.Parse(rawGitlabURL)
+	gitlabUrl := gitlab.Scheme + "://" + gitlab.Host
 	health := healthcheck.NewHandler()
 	health.AddLivenessCheck("goroutine-threshold", goroutine.Count(100))
 	database, err := database.GetConn().DB()
@@ -34,8 +41,30 @@ func newHandler() healthcheck.Handler {
 	}
 
 	health.AddReadinessCheck("database", db.Ping(database, 1*time.Second))
-	health.AddReadinessCheck("gitlab", healthHttp.Get("https://about.gitlab.com", 1*time.Second))
+	health.AddReadinessCheck("gitlab", Get(gitlabUrl, 1*time.Second))
 	return health
+}
+
+// Get was the function that allow follow the url
+func Get(url string, timeout time.Duration) checks.Check {
+	client := http.Client{
+		Timeout: timeout,
+	}
+
+	return func() error {
+		resp, err := client.Get(url)
+
+		if err != nil {
+			return err
+		}
+
+		resp.Body.Close()
+		if resp.StatusCode != 200 {
+			return fmt.Errorf("returned status %d", resp.StatusCode)
+		}
+
+		return nil
+	}
 }
 
 // HealthLiveHandler ...
