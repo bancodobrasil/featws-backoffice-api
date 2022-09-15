3bafa438001ba4d34ca5171109a5820dbbe8b5e7
Elias Feijo de Almeida Pereira Lima <eliasfeijo.nave@gmail.com>
2022-09-15 14:29:39 -0300

    feat: using VerifyAuthTokenMiddleware on /v1 routes

--- routes/api/v1/v1.go
+++ routes/api/v1/v1.go
@@ -1,11 +1,13 @@
 package v1
 
 import (
+	"github.com/bancodobrasil/featws-api/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
 // Router define routes the API V1
 func Router(router *gin.RouterGroup) {
+	router.Use(middlewares.VerifyAuthTokenMiddleware())
 	rulesheetsRouter(router.Group("/rulesheets"))
 	//rpcRouter(router.Group("/"))
 }
