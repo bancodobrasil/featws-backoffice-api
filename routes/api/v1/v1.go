db5a3d2d5d8baa219ba38855914a303e8d45aa5a
Eduarda de Castro Guterres <86486666+eduardacastro@users.noreply.github.com>
2023-05-18 09:55:06 -0300

    feat: writing comments in folder v1

--- routes/api/v1/v1.go
+++ routes/api/v1/v1.go
@@ -8,6 +8,7 @@ import (
 // Router define routes the API V1
 func Router(router *gin.RouterGroup) {
 
+	// This code is defining the routes for the API v1.
 	router.Use(goauthgin.Authenticate())
 	rulesheetsRouter(router.Group("/rulesheets"))
 	//rpcRouter(router.Group("/"))
