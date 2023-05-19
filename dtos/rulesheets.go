d2c04735ec525169f26d957c27dcf3e3a3e4cda5
Eduarda de Castro Guterres <86486666+eduardacastro@users.noreply.github.com>
2023-05-19 13:40:01 -0300

    feat: comments in models

--- dtos/rulesheets.go
+++ dtos/rulesheets.go
@@ -6,14 +6,31 @@ import (
 	v1 "github.com/bancodobrasil/featws-api/payloads/v1"
 )
 
-// Rule ...
+// Rule type is a struct with three fields: "Condition", "Value", and "Type", all of which are optional and have specific JSON tags.
+//
+// Property:
+//
+//   - Condition: is a string that represents the condition that needs to be met for the rule to be applied. It could be a comparison operator like ">", "<", ">=","<=", "=", or a logical operator like "AND", "OR", "NOT".
+//   - Value: is an interface type, which means it can hold any type of value. It's used to store the value of a rule, which can be a string, number, boolean, or any other data type depending on the specific rule being defined.
+//   - Type: is a string that specifies the type of the value. It can be used to indicate whether the value is a string, number, boolean, or any other data type. This property is optional and can be omitted.
 type Rule struct {
 	Condition string      `json:"condition,omitempty"`
 	Value     interface{} `json:"value,omitempty"`
 	Type      string      `json:"type,omitempty"`
 }
 
-// Rulesheet ...
+// Rulesheet type represents a set of rules and parameters for a system, including features and a version number.
+//
+// Property:
+//   - ID: is an unsigned integer that represents the unique identifier of a Rulesheet.
+//   - Name: is a string property of the Rulesheet struct. It represents the name of the rulesheet.
+//   - Description: is a string that provides a brief explanation or summary of what the Rulesheet is about. It can be used to give context to the Rulesheet and help users understand its purpose.
+//   - Slug: is a string that represents a unique identifier for the Rulesheet. It is typically used in URLs to identify and access a specific Rulesheet.
+//   - HasStringRule - HasStringRule is a boolean property that indicates whether or not the rulesheet contains a string rule. A string rule is a rule that involves comparing or manipulating strings.
+//   - Version - The version of the rulesheet. It could be a string or a number that represents the version number of the rulesheet. This is useful for tracking changes and updates to the rulesheet over time.
+//   - Features - Features is a pointer to a slice of maps, where each map represents a feature of the rulesheet. Each map contains key-value pairs where the key is a string representing the name of the feature and the value is an interface{} representing the value of the feature.
+//   - Parameters: a pointer to a slice of maps, where each map represents a parameter that can be used in the rules defined in the `Rules` property. Each mapcontains key-value pairs where the key is a string representing the name of the parameter and the value is an interface{}
+//   - Rules: property is a pointer to a map of string keys and interface values. This map represents the set of rules that are associated with the rulesheet. Each key in the map represents a unique rule identifier, and the corresponding value is an interface that can be usedto store any type of data. The use of `interface` allows for flexibility in the type of data that can be stored in the map.
 type Rulesheet struct {
 	ID            uint
 	Name          string
@@ -26,7 +43,8 @@ type Rulesheet struct {
 	Rules         *map[string]interface{}
 }
 
-// NewRulesheetV1 ...
+// NewRulesheetV1 takes in a payload of rulesheet and returns a DTO with the rules converted to a
+// specific format.
 func NewRulesheetV1(payload v1.Rulesheet) (dto Rulesheet, err error) {
 
 	dto = Rulesheet{
@@ -72,6 +90,7 @@ func NewRulesheetV1(payload v1.Rulesheet) (dto Rulesheet, err error) {
 	return
 }
 
+// The function takes in an interface and recursively builds a rule based on its type.
 func buildRule(v interface{}) (interface{}, error) {
 	switch value := v.(type) {
 	case []interface{}:
