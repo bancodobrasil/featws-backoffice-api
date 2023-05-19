1d94f26fe99dfea59b6a6bb0cf27399a676f587f
Eduarda de Castro Guterres <86486666+eduardacastro@users.noreply.github.com>
2023-05-19 13:44:58 -0300

    feat: comments in file connection

--- database/connection.go
+++ database/connection.go
@@ -12,17 +12,18 @@ import (
 
 var db *gorm.DB
 
-// GetConn ...
+// GetConn returns a pointer to a GORM db connection.
 func GetConn() *gorm.DB {
 	return db
 }
 
-// GetModel ...
+// GetModel returns a GORM db object for a given model.
 func GetModel(value interface{}) *gorm.DB {
 	return db.Model(value)
 }
 
-// ConnectDB ...
+// ConnectDB connects to a MySQL database using a configuration file and logs a message if
+// successful.
 func ConnectDB() {
 
 	cfg := config.GetConfig()
