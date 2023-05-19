b5b8fbb57ff7351adb1cf3b5157a711020195d93
Eduarda de Castro Guterres <86486666+eduardacastro@users.noreply.github.com>
2023-05-19 09:27:06 -0300

    feat: comments in repository

--- repository/repository.go
+++ repository/repository.go
@@ -64,6 +64,11 @@ type repository[T any] struct {
 	db *gorm.DB
 }
 
+// const is a block defining a set of constants that represent the different types of database
+// operations that can be performed by the repository. These constants are used as labels for tracing
+// and logging purposes, allowing for easier identification of the specific operation being performed.
+// For example, when a `Create` operation is performed, the `create` constant is used to label the
+// corresponding log or trace entry.
 const (
 	create = "repo-create"
 	find   = "repo-find"
@@ -73,24 +78,33 @@ const (
 	delete = "repo-delete"
 )
 
-// Create ...
+// Create it's a function thats creating a new entity of type T in the database. It first creates a new session
+// using the `newSession` function, which returns a new `gorm.DB` instance with a new context. Then, it
+// calls the `CreateInTransaction` function with the new context, the new session, and a pointer to the
+// entity to be created. Finally, it returns the result of the `CreateInTransaction` function, which is
+// an error if the creation fails.
 func (r *repository[T]) Create(ctx context.Context, entity *T) error {
 	db := r.newSession(ctx)
 	return r.CreateInTransaction(ctx, db, entity)
 }
 
-// CreateInTransaction ...
+// CreateInTransaction is a method of the repository struct that creates a new entity of type T in the
+// database within a transaction. It takes a context.Context object, a *gorm.DB object representing the
+// transaction, and a pointer to the entity to be created as input parameters.
 func (r *repository[T]) CreateInTransaction(ctx context.Context, db *gorm.DB, entity *T) error {
 	// add the span of database query on the root span of the context
 	span := utils.GenerateSpanTracer(ctx, create)
 	defer span()
 
+	// This code block creates a new entity of type T in the database using the Create method of the gorm.DB object.
+	// The Create method returns a *gorm.DB that provides information about the operation's result, including any errors encountered.
 	result := db.Create(&entity)
 	if result.Error != nil {
 		log.WithContext(ctx).Errorf("error on insert the result into model: %v", result.Error)
 		return result.Error
 	}
 
+	//The code verifies the success of an entity's database insertion by checking the number of affected rows.
 	if result.RowsAffected != 1 {
 		err := errors.New("error on insert not inserted")
 		log.WithContext(ctx).Error(err.Error())
@@ -100,19 +114,30 @@ func (r *repository[T]) CreateInTransaction(ctx context.Context, db *gorm.DB, en
 	return nil
 }
 
-// Find ...
+// Find is a method of the `repository` struct that finds a list of entities from the
+// repository based on specified criteria in the `FindOptions` parameter. It takes a `context.Context`
+// object, an `interface{}` object representing the entity to be found, and a pointer to a
+// `FindOptions` object representing the options for the find operation as input parameters. It returns
+// a slice of pointers to the type `T` and an error if any occurred during the operation.
 func (r *repository[T]) Find(ctx context.Context, entity interface{}, options *FindOptions) (list []*T, err error) {
 	db := r.newSession(ctx)
 	return r.FindInTransaction(ctx, db, entity, options)
 }
 
-// FindInTransaction ...
+// FindInTransaction is a method of a generic repository that finds a list of entities in a database
+// transaction using GORM. It takes in a context, a GORM database instance, an entity interface, and
+// optional find options. It starts a new span on the context for tracing purposes, applies any
+// specified find options to the database query, executes the query using GORM's Find method, and
+// returns the resulting list of entities or an error if there was one.
 func (r *repository[T]) FindInTransaction(ctx context.Context, db *gorm.DB, entity interface{}, options *FindOptions) (list []*T, err error) {
 	// add the span of database query on the root span of the context
 	tracer := telemetry.GetTracer(ctx)
 	ctx, span := tracer.Start(ctx, "repo-find", trace.WithSpanKind(trace.SpanKindInternal))
 	defer span.End()
 
+	// The code snippet above checks if the options parameter is not nil. If it's not nil, it assigns a
+	// default limit of 10 and verifies if the Limit field of options is not 0. If Limit is not 0, it assigns
+	// the value of options.Limit to the limit variable. Finally, it applies the limit to the database query using db.Limit(limit).
 	if options != nil {
 		limit := 10
 		if options.Limit != 0 {
@@ -124,6 +149,8 @@ func (r *repository[T]) FindInTransaction(ctx context.Context, db *gorm.DB, enti
 		}
 	}
 
+	// The code below queries the database using the Find method, searching for parameters corresponding
+	// to entity, and the results are returned in list.
 	result := db.Find(&list, entity)
 
 	err = result.Error
@@ -135,13 +162,19 @@ func (r *repository[T]) FindInTransaction(ctx context.Context, db *gorm.DB, enti
 	return
 }
 
-// Count ...
+// Count is a method that counts the number of entities in a db table. It takes a context and an entity as input parameters and returns the
+// count of entities and an error if any. It creates a new database session and calls the
+// CountInTransaction method passing the context, db session, and entity as parameters to count
+// the number of entities in the table.
 func (r *repository[T]) Count(ctx context.Context, entity interface{}) (count int64, err error) {
 	db := r.newSession(ctx)
 	return r.CountInTransaction(ctx, db, entity)
 }
 
-// CountInTransaction ...
+// CountInTransaction is a method that counts the number of records in a db table that match a given
+// entity. It takes a context, a db connection, and an entity as input parameters. It uses the OpenTelemetry
+// library to create a span for the db query and logs any errors that occur during the query. The method
+// returns the count of matching records and any errors encountered during the query.
 func (r *repository[T]) CountInTransaction(ctx context.Context, db *gorm.DB, entity interface{}) (count int64, err error) {
 	// add the span of database query on the root span of the context
 	tracer := telemetry.GetTracer(ctx)
@@ -150,6 +183,8 @@ func (r *repository[T]) CountInTransaction(ctx context.Context, db *gorm.DB, ent
 
 	count = 0
 
+	// The code is used to query a database using the Where method with a specific entity as a filter. It's
+	// used to determine the number of records that match the provided entity, and the result is stored in the count variable.
 	result := db.Where(entity).Count(&count)
 
 	err = result.Error
@@ -161,7 +196,8 @@ func (r *repository[T]) CountInTransaction(ctx context.Context, db *gorm.DB, ent
 	return
 }
 
-// Get ...
+// Get method is a function that takes a context and an ID as input parameters and returns a pointer to an
+// entity of type T and an error.
 func (r *repository[T]) Get(ctx context.Context, id string) (entity *T, err error) {
 	db := r.newSession(ctx)
 	return r.GetInTransaction(ctx, db, id)
